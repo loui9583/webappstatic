95065af9768943bf8293950988b9c89ce81f02d1
louis <loui9583@stud.kea.dk>
2023-09-24 15:18:28 +0200

    refactor

--- js/1.js
+++ js/1.js
@@ -1,58 +1,57 @@
-export function load() {
-    document.getElementById("1").style.background = "#2c1e1e"
-    document.getElementById("2").style.background = "none"
-    document.getElementById("3").style.background = "none"
-    document.getElementById("4").style.background = "none"
-    document.getElementById("5").style.background = "none"
+export async function load() {
+    document.getElementById("1").style.background = "#2c1e1e";
+    document.getElementById("2").style.background = "none";
+    document.getElementById("3").style.background = "none";
+    document.getElementById("4").style.background = "none";
+    document.getElementById("5").style.background = "none";
     document.getElementById('getUserById').addEventListener('click', () => fetchUserById(document.getElementById("userId").value));
     document.getElementById('getAllUsers').addEventListener('click', () => fetchAllUsers());
 
-    function fetchUserById(userId) {
-        fetch('https://jsonplaceholder.typicode.com/users/' + userId)
-            .then(res => {
-                if (!res.ok) {
-                    throw new Error("FAIL: " + res.status);
-                } else {
-                    return res.json();
-                }
-            })
-            .then(user => {
-                document.getElementById("tbody").innerHTML = `    
-          <tr>
-              <td>${user.id}</td>
-              <td>${user.name}</td>
-              <td>${user.email}</td>
-              <td>${user.phone}</td>
-              <td>${user.address.street},${user.address.suite},${user.address.city}</td>
-              <td>${user.phone}</td>
-              <td>${user.website}</td>
-          </tr>`
-            })
-            .catch(error => console.log(`ERROR  ${error}`));
+    async function fetchUserById(userId) {
+        try {
+            const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
+
+            if (!response.ok) {
+                throw new Error("FAIL: " + response.status);
+            }
+
+            const user = await response.json();
+            document.getElementById("tbody").innerHTML = `
+        <tr>
+            <td>${user.id}</td>
+            <td>${user.name}</td>
+            <td>${user.email}</td>
+            <td>${user.phone}</td>
+            <td>${user.address.street},${user.address.suite},${user.address.city}</td>
+            <td>${user.phone}</td>
+            <td>${user.website}</td>
+        </tr>`;
+        } catch (error) {
+            console.log(`ERROR: ${error}`);
+        }
     }
 
-    function fetchAllUsers() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(res => {
-                if (!res.ok) {
-                    throw new Error("FAIL: " + res.status);
-                } else {
-                    return res.json();
-                }
-            })
-            .then(data => {
-                document.getElementById("tbody").innerHTML = data.map(user => `    
-          <tr>
-              <td>${user.id}</td>
-              <td>${user.name}</td>
-              <td>${user.email}</td>
-              <td>${user.phone}</td>
-              <td>${user.address.street},${user.address.suite},${user.address.city}</td>
-              <td>${user.phone}</td>
-              <td>${user.website}</td>
-          </tr>`).join("");
-            })
-            .catch(error => console.log(`ERROR  ${error}`));
-    }
+    async function fetchAllUsers() {
+        try {
+            const response = await fetch('https://jsonplaceholder.typicode.com/users');
 
+            if (!response.ok) {
+                throw new Error("FAIL: " + response.status);
+            }
+
+            const data = await response.json();
+            document.getElementById("tbody").innerHTML = data.map(user => `
+        <tr>
+            <td>${user.id}</td>
+            <td>${user.name}</td>
+            <td>${user.email}</td>
+            <td>${user.phone}</td>
+            <td>${user.address.street},${user.address.suite},${user.address.city}</td>
+            <td>${user.phone}</td>
+            <td>${user.website}</td>
+        </tr>`).join("");
+        } catch (error) {
+            console.log(`ERROR: ${error}`);
+        }
+    }
 }
--- js/5.js
+++ js/5.js
@@ -1,223 +1,216 @@
-import {loadEditForms} from "./loadEditForm.js";
-//import {loadContent} from "./loadContent";
+import { loadEditForms } from "./loadEditForm.js";
 
-export function load() {
-
-//loadEditForms('editCarForm', 5);
-
-    function deleteMember(username) {
-        fetch(`http://localhost:8080/api/members/${username}`, { method: "delete" })
-            .then(res => {
-                if (res.ok) {
-                    console.log("Member deleted successfully");
-                    fetchAllUsers();
-                } else {
-                    throw new Error(`ERROR: Status: ${res.status}.`);
-                }
-            })
-            .catch(error => console.error(`ERROR: ${error}`));
-    }
-
-
-
-    document.getElementById("1").style.background = "none";
-    document.getElementById("2").style.background = "none";
-    document.getElementById("3").style.background = "none";
-    document.getElementById("4").style.background = "none";
-    document.getElementById("5").style.background = "#2c1e1e";
-
+export async function load() {
     const getByIdButton = document.getElementById("getById");
 
     function getInputId() {
         return document.getElementById("id").value;
     }
 
-    function fetchCarById(carId) {
-        fetch(`http://localhost:8080/api/cars/id/${carId}/includeAll`)
-            .then(res => {
-                if (!res.ok) {
-                    throw new Error("FAIL: " + res.status);
-                } else {
-                    return res.json();
-                }
-            })
-            .then(car => {
-                document.getElementById("tbody").innerHTML = `
-                <td>${car.id}</td>
-                <td>${car.brand}</td>
-                <td>${car.model}</td>
-                <td>${car.pricePrDay}</td>
-                <td>${car.bestDiscount}</td>
-                
-                <td><button style="margin-right: 10px;" onclick="" class="btn btn-primary edit-button">Edit</button>
-                <button class="btn btn-danger">Delete</button>
-                </td>`;
-            })
-            .catch(error => console.log(`ERROR: ${error}`));
+    async function deleteMember(username) {
+        try {
+            const response = await fetch(`http://localhost:8080/api/members/${username}`, {
+                method: "DELETE"
+            });
+
+            if (response.ok) {
+                console.log("Member deleted successfully");
+                await fetchAllUsers();
+            } else {
+                throw new Error(`ERROR: Status: ${response.status}.`);
+            }
+        } catch (error) {
+            console.error(`ERROR: ${error}`);
+        }
     }
 
-    function fetchMemberById(username) {
-        fetch(`http://localhost:8080/api/members/${username}`)
-            .then(res => {
-                if (!res.ok) {
-                    throw new Error("FAIL: " + res.status);
-                } else {
-                    return res.json();
-                }
-            })
-            .then(member => {
-                document.getElementById("tbody").innerHTML = `
-                <td>${member.username}</td>
-                <td>${member.email}</td>
-                <td>${member.firstName}</td>
-                <td>${member.lastName}</td>
-                <td>${member.street}</td>
-                <td>${member.city}</td>
-                <td>${member.zip}</td>
-                <td><button style="margin-right: 10px;" class="btn btn-primary">Edit</button>
-                <button class="btn btn-danger">Delete</button>
-                </td>`;
-            })
-            .catch(error => console.log(`ERROR: ${error}`));
+    async function fetchCarById(carId) {
+        try {
+            const response = await fetch(`http://localhost:8080/api/cars/id/${carId}/includeAll`);
+
+            if (!response.ok) {
+                throw new Error("FAIL: " + response.status);
+            }
+
+            const car = await response.json();
+            document.getElementById("tbody").innerHTML = `
+        <td>${car.id}</td>
+        <td>${car.brand}</td>
+        <td>${car.model}</td>
+        <td>${car.pricePrDay}</td>
+        <td>${car.bestDiscount}</td>
+        <td><button style="margin-right: 10px;" onclick="" class="btn btn-primary edit-button">Edit</button>
+        <button class="btn btn-danger">Delete</button>
+        </td>`;
+        } catch (error) {
+            console.log(`ERROR: ${error}`);
+        }
     }
 
-    function fetchAllCars() {
-        fetch('http://localhost:8080/api/cars/includeAll')
-            .then(res => {
-                if (!res.ok) {
-                    throw new Error("FAIL: " + res.status);
-                } else {
-                    return res.json();
-                }
-            })
-            .then(data => {
-                const tbody = document.getElementById("tbody");
-                tbody.innerHTML = '';
+    async function fetchMemberById(username) {
+        try {
+            const response = await fetch(`http://localhost:8080/api/members/${username}`);
 
-                data.forEach(car => {
-                    const editButton = document.createElement("button");
-                    editButton.style.marginRight = "10px";
-                    editButton.classList.add("btn", "btn-primary");
-                    editButton.textContent = "Edit";
-                    editButton.addEventListener("click", () => {
-                        loadEditForms('editCarForm', car.id);
-                    });
+            if (!response.ok) {
+                throw new Error("FAIL: " + response.status);
+            }
 
-                    const deleteButton = document.createElement("button");
-                    deleteButton.classList.add("btn", "btn-danger");
-                    deleteButton.textContent = "Delete";
+            const member = await response.json();
+            document.getElementById("tbody").innerHTML = `
+        <td>${member.username}</td>
+        <td>${member.email}</td>
+        <td>${member.firstName}</td>
+        <td>${member.lastName}</td>
+        <td>${member.street}</td>
+        <td>${member.city}</td>
+        <td>${member.zip}</td>
+        <td><button style="margin-right: 10px;" class="btn btn-primary">Edit</button>
+        <button class="btn btn-danger">Delete</button>
+        </td>`;
+        } catch (error) {
+            console.log(`ERROR: ${error}`);
+        }
+    }
 
-                    // Add event listener to the delete button
-                    deleteButton.addEventListener("click", () => {
-                        deleteCar(car.id); // Call the deleteCar function with the car's ID
-                    });
+    async function fetchAllCars() {
+        try {
+            const response = await fetch('http://localhost:8080/api/cars/includeAll');
 
-                    const td = document.createElement("td");
-                    td.appendChild(editButton);
-                    td.appendChild(deleteButton);
+            if (!response.ok) {
+                throw new Error("FAIL: " + response.status);
+            }
 
-                    const tr = document.createElement("tr");
-                    tr.innerHTML = `
-                    <td>${car.id}</td>
-                    <td>${car.brand}</td>
-                    <td>${car.model}</td>
-                    <td>${car.pricePrDay}</td>
-                    <td>${car.bestDiscount}</td>`;
-                    tr.appendChild(td);
+            const data = await response.json();
+            const tbody = document.getElementById("tbody");
+            tbody.innerHTML = '';
 
-                    tbody.appendChild(tr);
+            data.forEach(car => {
+                const editButton = document.createElement("button");
+                editButton.style.marginRight = "10px";
+                editButton.classList.add("btn", "btn-primary");
+                editButton.textContent = "Edit";
+                editButton.addEventListener("click", () => {
+                    loadEditForms('editCarForm', car.id);
                 });
-            })
-            .catch(error => console.log(`ERROR: ${error}`));
-    }
 
+                const deleteButton = document.createElement("button");
+                deleteButton.classList.add("btn", "btn-danger");
+                deleteButton.textContent = "Delete";
 
-    function fetchAllUsers() {
-        fetch('http://localhost:8080/api/members/includeAll')
-            .then(res => {
-                if (!res.ok) {
-                    throw new Error("FAIL: " + res.status);
-                } else {
-                    return res.json();
-                }
-            })
-            .then(data => {
-                const tbody = document.getElementById("tbody");
-                tbody.innerHTML = ''; // Clear the tbody first
-
-                data.forEach(user => {
-                    const editButton = document.createElement("button");
-                    editButton.style.marginRight = "10px";
-                    editButton.classList.add("btn", "btn-primary");
-                    editButton.textContent = "Edit";
-                    editButton.addEventListener("click", () => {
-                        loadEditForms('editUserForm', user.username); // Pass 'editUserForm' and username
-                    });
-
-                    const deleteButton = document.createElement("button");
-                    deleteButton.classList.add("btn", "btn-danger");
-                    deleteButton.textContent = "Delete";
-                    deleteButton.addEventListener("click", () => {
-                       deleteMember(user.username)
-                    });
-
-
-                    const td = document.createElement("td");
-                    td.appendChild(editButton);
-                    td.appendChild(deleteButton);
-
-                    const tr = document.createElement("tr");
-                    tr.innerHTML = `
-                    <td>${user.username}</td>
-                    <td>${user.email}</td>
-                    <td>${user.firstName}</td>
-                    <td>${user.lastName}</td>
-                    <td>${user.street}</td>
-                    <td>${user.city}</td>
-                    <td>${user.zip}</td>`;
-                    tr.appendChild(td);
-
-                    tbody.appendChild(tr);
+                deleteButton.addEventListener("click", () => {
+                    deleteCar(car.id);
                 });
-            })
-            .catch(error => console.log(`ERROR  ${error}`));
+
+                const td = document.createElement("td");
+                td.appendChild(editButton);
+                td.appendChild(deleteButton);
+
+                const tr = document.createElement("tr");
+                tr.innerHTML = `
+          <td>${car.id}</td>
+          <td>${car.brand}</td>
+          <td>${car.model}</td>
+          <td>${car.pricePrDay}</td>
+          <td>${car.bestDiscount}</td>`;
+                tr.appendChild(td);
+
+                tbody.appendChild(tr);
+            });
+        } catch (error) {
+            console.log(`ERROR: ${error}`);
+        }
     }
 
-    function deleteCar(carId) {
-        fetch(`http://localhost:8080/api/cars/${carId}`, {method: "delete"})
-            .then(res => {
-                if (res.ok) {
-                    console.log("Great Success");
-                    fetchAllCars();
-                } else throw new Error(`ERROR: Status: ${res.status}.`);
-            })
+    async function fetchAllUsers() {
+        try {
+            const response = await fetch('http://localhost:8080/api/members/includeAll');
+
+            if (!response.ok) {
+                throw new Error("FAIL: " + response.status);
+            }
+
+            const data = await response.json();
+            const tbody = document.getElementById("tbody");
+            tbody.innerHTML = '';
+
+            data.forEach(user => {
+                const editButton = document.createElement("button");
+                editButton.style.marginRight = "10px";
+                editButton.classList.add("btn", "btn-primary");
+                editButton.textContent = "Edit";
+                editButton.addEventListener("click", () => {
+                    loadEditForms('editUserForm', user.username);
+                });
+
+                const deleteButton = document.createElement("button");
+                deleteButton.classList.add("btn", "btn-danger");
+                deleteButton.textContent = "Delete";
+                deleteButton.addEventListener("click", () => {
+                    deleteMember(user.username);
+                });
+
+                const td = document.createElement("td");
+                td.appendChild(editButton);
+                td.appendChild(deleteButton);
+
+                const tr = document.createElement("tr");
+                tr.innerHTML = `
+          <td>${user.username}</td>
+          <td>${user.email}</td>
+          <td>${user.firstName}</td>
+          <td>${user.lastName}</td>
+          <td>${user.street}</td>
+          <td>${user.city}</td>
+          <td>${user.zip}</td>`;
+                tr.appendChild(td);
+
+                tbody.appendChild(tr);
+            });
+        } catch (error) {
+            console.log(`ERROR: ${error}`);
+        }
+    }
+
+    async function deleteCar(carId) {
+        try {
+            const response = await fetch(`http://localhost:8080/api/cars/${carId}`, { method: "DELETE" });
+
+            if (response.ok) {
+                console.log("Great Success");
+                await fetchAllCars();
+            } else {
+                throw new Error(`ERROR: Status: ${response.status}.`);
+            }
+        } catch (error) {
+            console.error(`ERROR: ${error}`);
+        }
     }
 
     function loadMembers() {
         document.getElementById("thead").innerHTML = `
-            <tr>
-                <th>Username</th>
-                <th>Email</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Street</th>
-                <th>City</th>
-                <th>Zip</th>
-                <th>Actions</th>
-            </tr>`;
+      <tr>
+        <th>Username</th>
+        <th>Email</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Street</th>
+        <th>City</th>
+        <th>Zip</th>
+        <th>Actions</th>
+      </tr>`;
         document.getElementById("id").placeholder = "Enter member Username";
     }
 
     function loadCars() {
         document.getElementById("thead").innerHTML = `
-            <tr>
-                <th>ID</th>
-                <th>Brand</th>
-                <th>Model</th>
-                <th>PricePrDay</th>
-                <th>BestDiscount</th>
-                <th>Actions</th>
-            </tr>`;
+      <tr>
+        <th>ID</th>
+        <th>Brand</th>
+        <th>Model</th>
+        <th>PricePrDay</th>
+        <th>BestDiscount</th>
+        <th>Actions</th>
+      </tr>`;
         document.getElementById("id").placeholder = "Enter Car ID";
     }
 
@@ -245,13 +238,9 @@ export function load() {
 
     document.getElementById("addNewCar").addEventListener("click", () => {
         loadEditForms("addCar");
-    })
+    });
 
     document.getElementById("addNewMember").addEventListener("click", () => {
         loadEditForms("addMember");
     });
-
-
-
-
 }
